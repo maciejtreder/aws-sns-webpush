3a96f07c9f431bdf40e153fe5774250c215b76e2
Maciej Treder <contact@maciejtreder.com>
2017-07-15 19:16:17 +0200

    adding builders

--- src/main/java/com/maciejtreder/aws/webpush/model/NotificationWrapper.java
+++ src/main/java/com/maciejtreder/aws/webpush/model/NotificationWrapper.java
@@ -1,8 +1,10 @@
 package com.maciejtreder.aws.webpush.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class NotificationWrapper {
     private String title;
     private String body;
--- src/main/java/com/maciejtreder/aws/webpush/model/Payload.java
+++ src/main/java/com/maciejtreder/aws/webpush/model/Payload.java
@@ -1,10 +1,12 @@
 package com.maciejtreder.aws.webpush.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class Payload {
     private NotificationWrapper notification;
 }
