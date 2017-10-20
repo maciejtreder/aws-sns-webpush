48c72a759e184444291ae300595d6ea08da8d424
Maciej Treder <contact@maciejtreder.com>
2017-10-20 10:39:51 +0200

    website address change

--- src/main/java/com/maciejtreder/aws/webpush/AwsWebPushApp.java
+++ src/main/java/com/maciejtreder/aws/webpush/AwsWebPushApp.java
@@ -46,7 +46,7 @@ public class AwsWebPushApp {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:8000", "https://www.angular-universal-serverless.maciejtreder.com");
+                registry.addMapping("/**").allowedOrigins("https://www.angular-universal-pwa.maciejtreder.com");
             }
         };
     }
